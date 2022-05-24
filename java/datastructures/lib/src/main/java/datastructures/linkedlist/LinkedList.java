c6454fe46ff1cb60792eb2353e11a7322454ec2d
Scott Evans <michealsevans20@gmail.com>
2022-05-23 23:03:13 -0500

    sff

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -1,10 +1,20 @@
 package datastructures.linkedlist;
 
-public class LinkedList
-{
+public class LinkedList {
+
+  Node head = null;
+  Node tail = null;
+
+  public void insert(int value) {
+//    TODO: implement me
+//    new Node()
+   }
+   public boolean includes(int value){
+      return false;
+   }
+
   @Override
-  public String toString()
-    {
-        return "null";  // TODO: implement me
-    }
+  public String toString() {
+    return "null";//ToDo: implement me
+  }
 }
--- java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
+++ java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
@@ -6,10 +6,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LinkedListTest
 {
-  @Test void testAMethod()
+  @Test void testInsertMethod()
     {
       LinkedList sut = new LinkedList();
       System.out.println(sut);
       assertTrue(true);
     }
+  @Test void testIncludesMethod()
+  {
+    LinkedList sut = new LinkedList();
+    System.out.println(sut);
+    assertTrue(true);
+  }
+  @Test void testToStringMethod()
+  {
+    LinkedList sut = new LinkedList();
+    System.out.println(sut);
+    assertTrue(true);
+  }
 }
